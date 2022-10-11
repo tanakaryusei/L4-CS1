2e40adb66e27479014bdc53b490404a6ad58791c
Ubuntu <ubuntu@ip-172-31-42-71.ec2.internal>
2022-10-11 05:18:42 +0000

    CS1

---
+++ app/controllers/top_controller.rb
@@ -0,0 +1,22 @@
+class TopController < ApplicationController
+    
+    def main
+        if session[:login_uid]
+            render "main"
+        else
+            render "login"
+        end
+    end
+    
+    def login
+        if params[:uid] =='kindai' and params[:pass] == 'sanriko'
+            session[:login_uid] = params[:uid]
+            redirect_to root_path
+        else
+            render 'error'
+        end
+        
+    end
+    
+end
+User.find_by(uid: params[:uid],pass: params[:pass])
